a55ad06ef27dd45d5f6dd1fe1682e24a58fba4f1
odoq <otto.hiltunenn@gmail.com>
2026-03-16 20:49:33 +0200

    Versio2

--- app/src/main/java/com/example/viikko9/AddNoteActivity.java
+++ app/src/main/java/com/example/viikko9/AddNoteActivity.java
@@ -1,6 +1,7 @@
 package com.example.viikko9;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +21,9 @@ public class AddNoteActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    public void addNote(View view) {
+
     }
 }
